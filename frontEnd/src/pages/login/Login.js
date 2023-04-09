7f3fd33d1c9fbf29205061186c487ce31a09a177
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2023-04-09 19:00:16 +0100

    create useLogin hook

--- frontEnd/src/pages/login/Login.js
+++ frontEnd/src/pages/login/Login.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
+//img's
 import presentation from '../../assets/presentation-640.png'
+//css
 import './login.css'
+//hooks
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await login(email, password);
   };
   return (
     <div className="login__page-wrapper">
@@ -34,7 +39,8 @@ const Login = () => {
             value={password}
           />
         </label>
-        <button type="submit">Submit</button>
+        <button disabled={isLoading} type="submit">Submit</button>
+        {error && <div className="error"> {error}</div>}
       </form>
       {/* <div className="login__img-wrapper">
           <img src={presentation}/>
