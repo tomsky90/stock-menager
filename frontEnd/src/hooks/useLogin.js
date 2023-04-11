64bb147a9d2f847f063ff967e529d4d13fd4439c
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2023-04-11 23:09:46 +0100

    change settings

--- frontEnd/src/hooks/useLogin.js
+++ frontEnd/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('https://stock-menager-back-end.onrender.com/api/user/login', {
+    const response = await fetch('/api/user/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
