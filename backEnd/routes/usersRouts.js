fb2d6948dac4021d1f2822197f6ef3132c5d3cc8
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2023-04-25 07:33:24 +0100

    protect create-user route in api

--- backEnd/routes/usersRouts.js
+++ backEnd/routes/usersRouts.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const requireAdminAuthorization = require('../middleware/requireAdminAuthorization')
+
+const requireAuth = require('../middleware/requireAuth');
+// const requireAdminAuthorization = require('../middleware/requireAdminAuthorization')
 
 // controller functions
 const {loginUser, signupUser } = require('../controllers/userController')
+
 
 //login route
 router.post('/login', loginUser);
 
 // sign up route
-router.post('/create-user',requireAdminAuthorization , signupUser);
+router.post('/create-user', requireAuth, signupUser);
 
 module.exports = router;
