16ec9bc2671fdee77b13e7ee3606736943795809
tomsky90 <79411793+tomsky90@users.noreply.github.com>
2023-05-13 08:43:15 +0100

    Update config.js

--- frontEnd/src/config.js
+++ frontEnd/src/config.js
@@ -1,5 +1,5 @@
 
-export const BASEURL = 'http://localhost:4000'
+export const BASEURL = 'https://stock-menager-back-end.onrender.com' 
 
 //  https://stock-menager-back-end.onrender.com
 // http://localhost:4000
